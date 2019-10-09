be6ed701a7fad1a135cd704c8a2a8630bd9c69f3
Kacper Koziol <kacperu770@gmail.com>
2019-10-09 23:25:39 +0200

    Develop login system. Done: checking login and passowrd in DB

--- login.php
+++ login.php
@@ -2,5 +2,42 @@
 
 require_once('config.php');
 
+if (isset($_POST['login'])) {
+    
+    $login = htmlspecialchars( $_POST['login'] );
 
+    $connect = @new mysqli($dbHost, $dbUser, $dbPassword, $dbName);
+
+    if ($connect->connect_errno != 0) {
+        echo "We have a problem with Database connect. Contact with support.";
+    }
+
+    $loginQuery = $connect->query("SELECT * FROM users WHERE login='$login'");
+    if (!$loginQuery) {
+        throw new Exception($connect->error);
+    }
+
+    $row = $loginQuery->fetch_assoc();
+
+    $passwordToVerify = $row['password'];
+
+    $loginCheck = $loginQuery->num_rows;
+    if ($loginCheck == 1) {
+        
+        $password = htmlspecialchars( $_POST['password'] );
+
+        if (password_verify($password, $row['password']) == true) {
+            echo "Hasło się zgadza. ZALOGOWANO do konta gracza " . $login;
+        }
+        else{
+            echo "Login ok, ale hasło chujowe.";
+        }
+
+    }
+    else{
+        echo "Nie ma takiego chłopa w DB, albo coś się zjebało z hasłem";
+    }
+
+
+}
 
