bee784382ed9c47e039ca8e13de0c97a3e4411ea
Kacper Koziol <kacperu770@gmail.com>
2019-10-10 21:15:30 +0200

    login system is finished

--- index.php
+++ index.php
@@ -1,5 +1,50 @@
 <?php
 session_start();
+
+require_once('config.php');
+
+if (isset($_POST['submit-btn']) && empty($_POST['login'])) {
+    $_SESSION['e_login'] = '<span style="color: red">Podaj login ciulu.</span><br>';
+}
+elseif (isset($_POST['login'])) {
+    
+    $login = htmlspecialchars( $_POST['login'] );
+
+    $connect = @new mysqli($dbHost, $dbUser, $dbPassword, $dbName);
+
+    if ($connect->connect_errno != 0) {
+        echo "We have a problem with Database connect. Contact with support.";
+    }
+
+    $loginQuery = $connect->query("SELECT * FROM users WHERE login='$login'");
+    if (!$loginQuery) {
+        throw new Exception($connect->error);
+    }
+
+    $row = $loginQuery->fetch_assoc();
+
+    $passwordToVerify = $row['password'];
+
+    $loginCheck = $loginQuery->num_rows;
+    if ($loginCheck == 1) {
+        
+        $password = htmlspecialchars( $_POST['password'] );
+
+        if (password_verify($password, $row['password']) == true) {
+            header("Location: app.php");
+
+            $connect->close();
+        }
+        else{
+            $_SESSION['e_password'] = '<span style="color: red">Błędne hasło.</span><br>';
+        }
+
+    }
+    else{    
+        $_SESSION['e_login'] = '<span style="color: red">Brak użytkownika o takim loginie.</span><br>';
+    }
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -10,12 +55,22 @@ session_start();
     </head>
     <body>
         <h2>Home | </h2> <a href="register.php"><h3>Register now!</h3></a>
-        <form action="login.php" method="POST">
+        <form action="index.php" method="POST">
             <label>Login</label><br>
-            <input type="text" name="login" placeholder="Login"><br><br>
+            <input type="text" name="login" placeholder="Login"><br>
+            <?php
+            if (isset($_SESSION['e_login'])) {
+                echo $_SESSION['e_login'];unset($_SESSION['e_login']);
+            }
+            ?><br>
             <label>Password</label><br>
-            <input type="password" name="password" placeholder="Password"><br><br>
-            <button type="submit">Login</button>
+            <input type="password" name="password" placeholder="Password"><br>
+            <?php
+                if (isset($_SESSION['e_password'])) {
+                    echo $_SESSION['e_password'];unset($_SESSION['e_password']);   
+                }             
+            ?><br>
+            <button type="submit" name="submit-btn">Login</button>
 
         </form>
 
