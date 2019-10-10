168d386e5cdec249e31d839112f5a27b40af44be
Kacper Koziol <kacperu770@gmail.com>
2019-10-10 23:49:37 +0200

    Create page, where you go after successful login. Add Logout button.

--- app.php
+++ app.php
@@ -1,7 +1,35 @@
 <?php
+session_start();
+require_once('config.php');
 
-echo "Hasło się zgadza. ZALOGOWANO do konta gracza <br><br>";
+if (!$_SESSION['logged']) {
+    header("Location: index.php");
+}
 
-echo "Game main file";
+$userID = $_SESSION['id'];
+$userLogin = $_SESSION['login'];
+
+echo "Zalogowano do konta gracza " . "<span style='color: blue'><strong>$userLogin</strong></span> ". "<br><br>";
+
+
+if (isset($_POST['logout-btn'])) {
+    header("Location: index.php");
+    session_destroy();
+}
 
 ?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>GRA</title>
+        <meta charset="UTF-8">
+    </head>
+
+    <body>
+    <form action="app.php" method="POST">
+        <button type="submit" name="logout-btn">Logout</button>
+    </form>
+
+    </body>
+</html>
