ee1b4eb3da2bfc159b41740e4d1d25fbe5947720
Kacper Koziol <kacperu770@gmail.com>
2019-10-18 00:07:23 +0200

    Add new file. I have no idea what game is this ;)

---
+++ startgame.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+require_once('config.php');
+
+echo "GRAMY!";
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>GRA</title>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="style.css">   
+    </head>
+    <body>
+        <br>
+        <a href="app.php">Menu główne</a>
+    </body>
+</html>
