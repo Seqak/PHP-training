60fab9f12ee25e3e506612b8c25fee2420c116a6
Kacper Koziol <kacperu770@gmail.com>
2019-10-22 22:54:43 +0200

    Connect with openweathermap API

--- startgame.php
+++ startgame.php
@@ -2,7 +2,16 @@
 session_start();
 require_once('config.php');
 
-echo "GRAMY!";
+$miasto = "Gdansk";
+
+$jsonData = file_get_contents('https://api.openweathermap.org/data/2.5/weather?q=' . $miasto . '&units=metric&appid=cf833ea4d9b83ed6d740d22700d79d1b');
+
+$json = json_decode($jsonData, true);
+
+echo "City: " . $json['name'] . "<br>";
+echo "Temp: " . $json['main']['temp'] . "<br>";
+
+echo "<br><br>" . "GRAMY!";
 ?>
 
 <!DOCTYPE html>
@@ -13,6 +22,7 @@ echo "GRAMY!";
         <link rel="stylesheet" href="style.css">   
     </head>
     <body>
+
         <br>
         <a href="app.php">Menu główne</a>
     </body>
